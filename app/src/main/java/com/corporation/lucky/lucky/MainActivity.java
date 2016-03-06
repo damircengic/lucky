77c963bca9f7115b4fad9d2070bcf5fd0b6113a7
Adi <begoli.adnan@gmail.com>
2016-03-06 23:36:04 +0100

    First commit najsuu~~~~u

--- app/src/main/java/com/corporation/lucky/lucky/MainActivity.java
+++ app/src/main/java/com/corporation/lucky/lucky/MainActivity.java
@@ -3,6 +3,8 @@ package com.corporation.lucky.lucky;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
